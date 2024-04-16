762a04e256228e87257cca10d93875dbd23d5e2b
liuydd <1594807461@qq.com>
2024-04-16 23:36:19 +0800

    实现转让群主的组件，群主可从群聊用户中选择用户转让群主 #19

---
+++ src/pages/group/assign_manager.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { BACKEND_URL, FAILURE_PREFIX} from "../../constants/string";
+import { useRouter } from "next/router";
+import { setName, setPassword, setToken } from "../../redux/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+
+function AssignManager() {
+    
+};
+export default AssignManager;
