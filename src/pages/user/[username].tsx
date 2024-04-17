baa5d4daca714db56a01e50947a2a678b4a05831
liuydd <1594807461@qq.com>
2024-04-17 22:58:27 +0800

    修改了用户主界面的布局 #24

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -12,12 +12,13 @@ import DeleteFriend from "../friends/delete";
 import ListFriends from "../friends/list";
 import LabelFriends from "../friends/label";
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme, Button, Form, Input, Modal } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Button, Form, Input, Modal, Image } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 import React from 'react';
 import Link from 'next/link';
 import { LogoutOutlined, DeleteOutlined } from '@ant-design/icons';
-import BasicLayout from "../../components/BasicLayout";
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
 
 const UserScreen = () => {
     //获取现有的userName, token, password
@@ -152,106 +153,77 @@ const UserScreen = () => {
     ];
 
     return (
-        <>
-            {/* <BasicLayout logout={logout} deleteUser={deleteUser}> 由于layout有点问题，故暂时还是按原来的写*/}
-            <p>
-                <button onClick = {logout}>
-                    Logout
-                </button>
-            </p>
-            <p>
-                <button onClick = {deleteUser}>
-                    Delete Account
-                </button>
-            </p>
-            <div>
-                <button onClick = {openModal}>
-                    Account Info
-                </button>
-                {showModal && (
-                <div className="modal">
-                <div className="modal-content">
-                    <span className="close" onClick={closeModal}>&times;</span>
-                    <h2>Edit Account Info</h2>
-                    <form onSubmit={edit}>
-                    <label>
-                        Username:
-                        <input
-                        type="text"
-                        name="username"
-                        value={username}
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        Password:
-                        <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={handleChange}
-                        />
-                    </label>
-                    {/* <label>
-                        New Username:
-                        <input
-                        type="text"
-                        name="newUsername"
-                        value={formData.newUsername}
-                        onChange={handleChange}
-                        />
-                    </label> */}
-                    <label>
-                        New Password:
-                        <input
-                        type="password"
-                        name="newPassword"
-                        value={formData.newPassword}
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        Avatar:
-                        <input
-                        type="file"
-                        name="avatar"
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        Email:
-                        <input
-                        type="email"
-                        name="newEmail"
-                        value={formData.newEmail}
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <label>
-                        Phone Number:
-                        <input
-                        type="tel"
-                        name="newPhoneNumber"
-                        value={formData.newPhoneNumber}
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <button type="submit">Save</button>
-                    </form>
+        <Layout>
+            <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div>
+                    <Button type="primary" icon={<LogoutOutlined />} onClick={logout}>Logout</Button>
+                    <Button type="primary" icon={<DeleteOutlined />} onClick={deleteUser}>Delete Account</Button>
                 </div>
+            </Header>
+            <Layout>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: 'calc(100vh - 64px)', // 减去 Header 的高度
+                    position: 'sticky',
+                    top: '64px', // Header 的高度
+                    left: 0,
+                    //background: colorBgContainer,
+                }}
+                width={200}
+                >
+                <div>
+                    <Avatar
+                        size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                        icon={<UserOutlined />}
+                    />
                 </div>
-            )}
-            </div>
-            {/* </BasicLayout> */}
-            <p><SearchUser /></p>
-            {/* 暂时这样列 */}
-            {/* <div><p><DeleteFriend /></p></div> */}
-            <div><p><ListFriends /></p></div>
-            <div><p><LabelFriends /></p></div>
-            <div><p><FriendRequest /></p></div>
-            <div><p><Respond2FriendRequest /></p></div>
-            <div><ListFriendRequests /></div>
-        </>
+                <div>
+                    <Button onClick={openModal}>Account Info</Button>
+                    {showModal && (
+                        <Modal
+                            visible={showModal}
+                            onCancel={closeModal}
+                            footer={null}
+                        >
+                            <h2>Edit Account Info</h2>
+                            <Form onFinish={edit}>
+                                <Form.Item label="New Username">
+                                    <Input name="newUsername" value={formData.newUsername} onChange={handleChange} />
+                                </Form.Item>
+                                <Form.Item label="New Password">
+                                    <Input.Password name="newPassword" value={formData.newPassword} onChange={handleChange} />
+                                </Form.Item>
+                                <Form.Item label="New Email">
+                                    <Input name="newEmail" value={formData.newEmail} onChange={handleChange} />
+                                </Form.Item>
+                                <Form.Item label="New Phone Number">
+                                    <Input name="newPhoneNumber" value={formData.newPhoneNumber} onChange={handleChange} />
+                                </Form.Item>
+                                <Button type="primary" htmlType="submit">Save</Button>
+                            </Form>
+                        </Modal>
+                    )}
+                </div>
+                <div className="demo-logo-vertical" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
+                    {items.map((item) => (
+                    <Menu.Item key={item.key}>
+                        <Link href={item.to}>{item.label}</Link>
+                    </Menu.Item>
+                    ))}
+                </Menu>
+                </Sider>
+                <Content>
+                    <SearchUser />
+                    {/* <ListFriends /> */}
+                    <LabelFriends />
+                    <FriendRequest />
+                    <Respond2FriendRequest />
+                    {/* <ListFriendRequests /> */}
+                </Content>
+            </Layout>
+        </Layout>
     );
 };
 
