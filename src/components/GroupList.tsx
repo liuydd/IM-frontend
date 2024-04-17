c732a50b94f5f3f5a985a164a02f14fd054f83e7
liuydd <1594807461@qq.com>
2024-04-17 15:36:47 +0800

    暂时实现grouplist的交互功能，具体返回的组件或页面待写 #24

---
+++ src/components/GroupList.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { BACKEND_URL, FAILURE_PREFIX } from "../constants/string";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { Card, Divider } from "antd";
+
+function GroupList() {
+  //const userid = useSelector((state: RootState) => state.auth.userid);
+  const username = useSelector((state: RootState) => state.auth.name);
+  const [groups, setGroups] = useState({
+    monitorGroup: [],
+    manageGroup: [],
+    memberOfGroup: []
+  });
+
+  useEffect(() => {
+    //fetch(`${BACKEND_URL}/api/group/list_group?userid=${userid}`)
+    fetch(`${BACKEND_URL}/api/group/list?username=${username}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (Number(res.code) === 0) {
+          setGroups(res);
+        } else {
+          alert(res.info);
+        }
+      })
+      .catch((err) => alert(FAILURE_PREFIX + err));
+  });
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default GroupList;
