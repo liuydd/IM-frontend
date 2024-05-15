4d09335fb0f17c69aa5c210150841ca413630f73
liuydd <1594807461@qq.com>
2024-05-15 10:09:40 +0800

    解决了将IM demo迁移过来build失败的问题

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     output: "standalone",
     reactStrictMode: false, /* @note: To prevent duplicated call of useEffect */
     swcMinify: true,
+    transpilePackages: ['ahooks'],
 
     async rewrites() {
         return [{
