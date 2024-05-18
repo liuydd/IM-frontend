8be23d5324046ae3b3a83b174bbbcff2f4a1746f
liuydd <1594807461@qq.com>
2024-05-18 11:54:58 +0800

    将本地测试用的配置改回在平台上的测试配置

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
         return [{
             source: "/api/:path*",
             // Change to your backend URL in production
-            // destination: "https://backend-fishandchips.app.secoder.net/:path*",
-            destination: "http://127.0.0.1:8000/:path*",
+            destination: "https://backend-fishandchips.app.secoder.net/:path*",
+            // destination: "http://127.0.0.1:8000/:path*",
         }];
     }
 };
