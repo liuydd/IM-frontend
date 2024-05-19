bcc224ccce8316bc0f78148355c50ffd870694ec
Sakila <root@LAPTOP-N5PBRLST.localdomain>
2024-05-19 12:27:54 +0800

    fix ws bug #29

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,3 @@
 export const API_BASE_URL = 'https://backend-fishandchips.app.secoder.net'; // 定义API基础URL
 // export const API_BASE_URL = 'http://localhost:8000';
+export const WS_BASE_URL = 'wss://backend-fishandchips.app.secoder.net/ws/?username='
